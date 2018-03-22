c11026bfc0498cd7c75d2844f63d81579e9a64c8
Justin Lafferty <lafferjm@gmail.com>
2018-03-21 20:04:02 -0500

    added level factory.  need to update mainscreen to use it.

--- core/src/net/lafferjm/gamedevbox2dtutorial/entity/components/PlayerComponent.java
+++ core/src/net/lafferjm/gamedevbox2dtutorial/entity/components/PlayerComponent.java
@@ -1,11 +1,12 @@
 package net.lafferjm.gamedevbox2dtutorial.entity.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 /**
  * Created by laffe on 3/20/2018.
  */
 
 public class PlayerComponent implements Component {
-
+    public OrthographicCamera cam = null;
 }
